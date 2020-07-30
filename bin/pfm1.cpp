e8ce116ec33ec2f8f868c83c2128600610b2cac0
Carl Pearson <carl.w.pearson@gmail.com>
2020-07-30 09:08:37 -0600

    .

--- bin/pfm1.cpp
+++ bin/pfm1.cpp
@@ -6,6 +6,8 @@
 #include <cstdio>
 #include <cstdlib>
 #include <cstring>
+#include <string>
+#include <vector>
 
 void list_pmu_events(pfm_pmu_t pmu) {
   pfm_event_info_t info;
@@ -18,7 +20,6 @@ void list_pmu_events(pfm_pmu_t pmu) {
   info.size = sizeof(info);
   pinfo.size = sizeof(pinfo);
 
-
   ret = pfm_get_pmu_info(pmu, &pinfo);
   if (ret != PFM_SUCCESS) {
     fprintf(stderr, "cannot get pmu info %s\n", pfm_strerror(ret));
@@ -30,38 +31,43 @@ void list_pmu_events(pfm_pmu_t pmu) {
     if (ret != PFM_SUCCESS)
       exit(1); //, "cannot get event info");
 
-    printf("Event: %s::%s\n", pinfo.name, info.name);
+    std::vector<std::string> names;
+
+    // printf("Event: %s::%s\n", pinfo.name, info.name);
     for (int attr = 0; attr < info.nattrs; ++attr) {
       pfm_event_attr_info_t eventAttr;
       std::memset(&eventAttr, 0, sizeof(eventAttr));
-      ret = pfm_get_event_attr_info(info.idx, attr, PFM_OS_PERF_EVENT, &eventAttr);
+      ret = pfm_get_event_attr_info(info.idx, attr, PFM_OS_PERF_EVENT,
+                                    &eventAttr);
       if (ret != PFM_SUCCESS) {
         fprintf(stderr, "  ATTR: error: %s\n", pfm_strerror(ret));
       } else if (eventAttr.type == PFM_ATTR_UMASK) {
-        printf("umask: %s\n", eventAttr.name);
+        // printf("umask: %s\n", eventAttr.name);
+        names.push_back(std::string(info.name) + ":" + eventAttr.name);
       } else {
-        printf("other attr: %s\n", eventAttr.name);
+        printf("other attr: %s %s\n", eventAttr.name, eventAttr.desc);
       }
-
     }
-    printf("nattrs=%ld\n", info.nattrs);
 
-    struct perf_event_attr pe;
-    std::memset(&pe, 0, sizeof(struct perf_event_attr));
+    for (auto &name : names) {
+      printf("%s\n", name.c_str());
+      struct perf_event_attr pe;
+      std::memset(&pe, 0, sizeof(struct perf_event_attr));
 
-    pfm_perf_encode_arg_t pea;
-    std::memset(&pea, 0, sizeof(pea));
-    pea.attr = &pe;
-    pea.size = sizeof(pea);
+      pfm_perf_encode_arg_t pea;
+      std::memset(&pea, 0, sizeof(pea));
+      pea.attr = &pe;
+      pea.size = sizeof(pea);
 
-    // https://man7.org/linux/man-pages/man3/pfm_get_os_event_encoding.3.html
-    ret = pfm_get_os_event_encoding(info.name, PFM_PLM0 | PFM_PLM3,
-                                    PFM_OS_PERF_EVENT, &pea);
-    if (ret != PFM_SUCCESS) {
-      fprintf(stderr, "cannot get encoding: %s\n", pfm_strerror(ret));
-      return;
+      // https://man7.org/linux/man-pages/man3/pfm_get_os_event_encoding.3.html
+      ret = pfm_get_os_event_encoding(name.c_str(), PFM_PLM0 | PFM_PLM3,
+                                      PFM_OS_PERF_EVENT, &pea);
+      if (ret != PFM_SUCCESS) {
+        fprintf(stderr, "cannot get encoding: %s\n", pfm_strerror(ret));
+        continue;
+      }
+      printf("config=%llu, config1=%llu, config2=%llu\n", pe.config);
     }
-    printf("config=%llu, config1=%llu, config2=%llu\n", pe.config);
   }
 }
 
